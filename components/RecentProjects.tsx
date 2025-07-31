f34e71b81e7e7734837af804d11f652dc615c61b
Ralein Nova <raleinnova@gmail.com>
2025-07-31 23:53:48 +0530

    updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -22,7 +22,7 @@ const RecentProjects = () => {
               title={item.title}
               href={item.link}
             >
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[34vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[27vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl backdrop-blur-lg border border-white/20 bg-black-200"
                 >
